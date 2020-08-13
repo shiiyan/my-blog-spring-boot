2fa9b7e47a7c857d613c0549087d08a0c4ad19d0
shiiyan <yan@softbank126074195022.bbtec.net>
2020-08-13 15:20:25 +0900

    add ktlink with gradle

--- blog/src/main/kotlin/com/example/blog/BlogApplication.kt
+++ blog/src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -1,10 +1,19 @@
 package com.example.blog
 
+import blog.ArticleRepository
+import blog.UserRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class BlogApplication
+class BlogApplication {
+	@Bean
+	fun databaseInitializer(userReposityory: UserRepository, articleRepository: ArticleRepository) {}
+}
+
+
+
 
 fun main(args: Array<String>) {
 	runApplication<BlogApplication>(*args)
--- blog/src/main/kotlin/com/example/blog/HtmlController.kt
+++ blog/src/main/kotlin/com/example/blog/HtmlController.kt
@@ -1,16 +1,31 @@
 package com.example.blog
 
+import blog.Article
+import blog.ArticleRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import java.lang.IllegalArgumentException
 
 @Controller
-class HtmlController {
+class HtmlController(private val repository: ArticleRepository) {
 
     @GetMapping("/")
     fun blog(model: Model): String {
         model["title"] = "Blog"
+        model["articles"] = repository.findAllByOrderByAddedAtDesc()
         return "blog"
+    }
+
+    @GetMapping("/article/{id}")
+    fun article(@PathVariable id: Long, model: Model): String {
+        val article = repository.findById(id).orElseThrow { IllegalArgumentException("Wrong article id provided") }
+
+        model["title"] = article.title
+        model["article"] = article
+
+        return "article"
     }
 }
